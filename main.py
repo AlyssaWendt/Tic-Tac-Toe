7ac16ea53e89a42b039611b362bb8d218a890b0d
Alyssa Wendt <83786185+awendt14@users.noreply.github.com>
2022-03-04 18:27:43 +0000

    gitadd

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import math
 
 # this class holds the data on what the game will look like
 class Design:
-  def __init__(self):
-    pygame.init()
-    pygame.display.set_caption("Alyssa's Awesome Sauce Tic Tac Toe Game")
+  WIDTH = 600
+  HEIGHT = 600
+  BKG = (34, 100, 186)
+  LINE_COLOR = (43, 186, 214)
+  BOARD_ROWS = 15
+  Board_COL = 15
+  SQUARE_SIZE = 200
+  LINE_WIDTH = 15
+  SPACE = 55
+  
+  
+  pygame.init()
     
-  def show_screen():    
-    WIDTH = 600
-    HEIGHT = 600
-    BKG = (66, 144, 245) 
+    
+  def show_screen(): 
+    pygame.display.set_caption("Alyssa's Awesome Sauce Tic Tac Toe Game")
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill(BKG)
-    pygame.display.flip()
+ 
+
+  def draw_board():
+    # horizontal lines
+    pygame.line(screen, LINE_COLOR, (0,SQUARE_SIZE),(SQUARE_SIZE, WIDTH))
+    pygame.line(screen, LINE_COLOR, (0, 2 * SQUARE_SIZE), (WIDTH, 2 * SQUARE_SIZE), LINE_WIDTH )
+
+    # vertical lines
+    pygame.line(screen, LINE_COLOR, (SQUARE_SIZE,0),(SQUARE_SIZE, HEIGHT))
+    pygame.line(screen, LINE_COLOR, (0, 2 *  SQUARE_SIZE ), ( 2 * SQUARE_SIZE, HEIGHT), LINE_WIDTH )
+
+    
+
 
 # this class holds the data on how the game will act 
 class Functionality:
