9f265db364e1c88126a897abb528ab46d8577806
Alyssa Wendt <83786185+awendt14@users.noreply.github.com>
2022-03-04 21:19:59 +0000

    gitadd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, sys
 from pygame.locals import *
-import math
+import numpy as np
 
 # this class holds the data on what the game will look like
 class Design:
@@ -8,8 +8,8 @@ class Design:
   HEIGHT = 600
   BKG = (34, 100, 186)
   LINE_COLOR = (43, 186, 214)
-  BOARD_ROWS = 15
-  Board_COL = 15
+  BOARD_ROWS = 3
+  Board_COL = 3
   SQUARE_SIZE = 200
   LINE_WIDTH = 15
   SPACE = 55
@@ -22,17 +22,8 @@ class Design:
     pygame.display.set_caption("Alyssa's Awesome Sauce Tic Tac Toe Game")
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill(BKG)
+    board = np.zeros( (BOARD_ROWS, BOARD_COLS) )
  
-
-  def draw_board():
-    # horizontal lines
-    pygame.line(screen, LINE_COLOR, (0,SQUARE_SIZE),(SQUARE_SIZE, WIDTH))
-    pygame.line(screen, LINE_COLOR, (0, 2 * SQUARE_SIZE), (WIDTH, 2 * SQUARE_SIZE), LINE_WIDTH )
-
-    # vertical lines
-    pygame.line(screen, LINE_COLOR, (SQUARE_SIZE,0),(SQUARE_SIZE, HEIGHT))
-    pygame.line(screen, LINE_COLOR, (0, 2 *  SQUARE_SIZE ), ( 2 * SQUARE_SIZE, HEIGHT), LINE_WIDTH )
-
     
 
 
@@ -44,3 +35,15 @@ class Functionality:
 	    for event in pygame.event.get():
 		    if event.type == pygame.QUIT:
 			    sys.exit()
+
+  def draw_board():
+    # horizontal lines
+    pygame.line(screen, LINE_COLOR, (0,SQUARE_SIZE),(SQUARE_SIZE, WIDTH))
+    pygame.line(screen, LINE_COLOR, (0, 2 * SQUARE_SIZE), (WIDTH, 2 * SQUARE_SIZE), LINE_WIDTH )
+
+    # vertical lines
+    pygame.line(screen, LINE_COLOR, (SQUARE_SIZE,0),(SQUARE_SIZE, HEIGHT))
+    pygame.line(screen, LINE_COLOR, (0, 2 *  SQUARE_SIZE ), ( 2 * SQUARE_SIZE, HEIGHT), LINE_WIDTH )
+
+
+
